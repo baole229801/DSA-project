63316eb8c2dda9d8fb667580c79441c38964c4c1
baoletrg <baole2298@gmail.com>
2019-06-01 20:40:19 +0700

    add key pressing method

--- sketch.js
+++ sketch.js
@@ -14,39 +14,65 @@ const updateCanvas = () => {
     drawGrid();
 }
 
-const drawGrid = () => {
-    let w = 100;
+const keyPressed = () => {
+    let flipped = false;
+    let rotated = false;
+    let played = true;
+    switch (keyCode) {
+        case DOWN_ARROW:
+            // do nothing
+            break;
+        case UP_ARROW:
+            grid = flipGrid(grid);
+            flipped = true;
+            break;
+        case RIGHT_ARROW:
+            grid = transposeGrid(grid);
+            rotated = true;
+            break;
+        case LEFT_ARROW:
+            grid = transposeGrid(grid);
+            grid = flipGrid(grid);
+            rotated = true;
+            flipped = true;
+            break;
+        default:
+            played = false;
+    }
 
-    for (let i = 0; i < 4; i++) {
-        for (let j = 0; j < 4; i++) {
-            noFill(); // grid separate not fill
-            strokeWeight(2) // thickness of border
-            let val = grid[i][j];
-            let s = val.toString();
+    const drawGrid = () => {
+        let w = 100;
 
-            if (grid_new[i][j] === 1) {
-                stroke(200, 0, 200);
-                strokeWeight(16);
-                grid_new[i][j] = 0;
-            } else {
-                strokeWeight(4);
-                stroke(0);
-            }
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; i++) {
+                noFill(); // grid separate not fill
+                strokeWeight(2) // thickness of border
+                let val = grid[i][j];
+                let s = val.toString();
 
-            if (val != 0) {
-                fill(colorSizes[s].color);
-            } else {
-                noFill();
-            }
+                if (grid_new[i][j] === 1) {
+                    stroke(200, 0, 200);
+                    strokeWeight(16);
+                    grid_new[i][j] = 0;
+                } else {
+                    strokeWeight(4);
+                    stroke(0);
+                }
 
-            rect(i * w, j * w, w, w, 30);
-            if (val !== 0) {
-                textAlign(CENTER, CENTER);
-                noStroke();
-                fill(0);
-                textSize(colorSizes[s].size);
-                text(val, i * w + w / 2, j * w + w / 2);
+                if (val != 0) {
+                    fill(colorSizes[s].color);
+                } else {
+                    noFill();
+                }
+
+                rect(i * w, j * w, w, w, 30);
+                if (val !== 0) {
+                    textAlign(CENTER, CENTER);
+                    noStroke();
+                    fill(0);
+                    textSize(colorSizes[s].size);
+                    text(val, i * w + w / 2, j * w + w / 2);
+                }
             }
         }
     }
-}
